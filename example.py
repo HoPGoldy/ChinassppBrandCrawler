da317a51aa97e7497f8e17b631175351e8e3b4cb
HoPGoldy <hpgless@outlook.com>
2018-09-01 09:24:53 +0800

    Update example.py

--- example.py
+++ example.py
@@ -4,11 +4,17 @@
 
 from ChinassppControl import Chinasspp
 
-if __name__ == '__main__':
+
+def searchBrand(brandName):
     chinasspp = Chinasspp()
-    result = chinasspp.searchBrand('zara')
+    result = chinasspp.searchBrand(brandName)
 
     if result is not None:
         for item in result:
             print(f'[名称] {item["name"]}')
             print(f'[介绍] {item["introduce"]}')
+            print(f'[logo] {item["logo"]}')
+
+
+if __name__ == '__main__':
+    searchBrand('zara')
